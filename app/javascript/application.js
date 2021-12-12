1ad5a12053d7b1df4df14819a8dd4c6ac2504be7
Cyril Suzat <cyril.suzat@me.com>
2021-12-12 11:26:41 +0100

    Setting up the layout with retractable sidebar (offcanvas)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,36 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Sidebar (offcanvas)
+
+const btnShowSidebar = document.querySelector("#btn-show-sidebar");
+btnShowSidebar.addEventListener('click', showSidebar);
+
+const btnHideSidebar = document.querySelector("#btn-hide-sidebar");
+btnHideSidebar.addEventListener('click', hideSidebar);
+
+function showSidebar(event) {
+  event.preventDefault();
+
+  let sidebar = document.querySelector('#sidebar');
+  sidebar.classList.add('visible');
+
+  let backdrop = document.createElement('div');
+  backdrop.id = 'backdrop';
+  backdrop.addEventListener('click', hideSidebar);
+  sidebar.after(backdrop);
+  backdrop.focus();
+  backdrop.classList.add('visible');
+}
+
+function hideSidebar(event) {
+  event.preventDefault();
+
+  let sidebar = document.querySelector('#sidebar');
+  sidebar.classList.remove('visible');
+
+  let backdrop = document.querySelector('#backdrop');
+  backdrop.classList.remove('visible');
+  setTimeout(() => { backdrop.remove(); }, 300);
+}
